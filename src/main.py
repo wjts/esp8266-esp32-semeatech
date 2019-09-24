d7332f61ba5605806f124cce16f7bdc9213c4d15
Wojciech Wojciechowski <w.wojciechowski@nobiles2.pl>
2019-09-24 07:19:00 +0200

    remove unnecessary else block in test code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from machine import Pin, I2C
 
 # esp32
-i2c = I2C(scl=Pin(22), sda=Pin(21), freq=10000)
+i2c = I2C(scl=Pin(22), sda=Pin(21), freq=100000)
 
 # esp8266
 # i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)
@@ -11,5 +11,3 @@ sensors = dict([(0x76, 'bme280'), (0x0B, 'semea'), (0x23, 'bh1750')])
 for address in i2c.scan():
     print('Detected sensor at 0x{:02X}: {}'.format(
         address, sensors.get(address, 'no data')))
-else:
-    print('No sensors detected')
